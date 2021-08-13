5081e4ea6744cb2b415fc0c640b05ec3ff4418a5
Renata Nunes <renatanunes231@yahoo.com.br>
2021-08-13 17:53:36 -0300

    Exercise 1 - completo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      },
+      car: '',
+      side: false,
+    };
+
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, side) {
+    this.setState({
+      cars: { ...this.state, [car]: side },
+    });
+  }
+
+  render() {
+    const { cars } = this.state;
+    const contextValue = {
+      cars,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={ contextValue }>
+        <div>
+          <Cars cars={ this.state.cars } moveCar={ this.moveCar } />
+        </div>
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
